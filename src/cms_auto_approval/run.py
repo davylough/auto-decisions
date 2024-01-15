90d04095e73277646c28c4f463f6d3d41c754fd9
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-15 15:45:15 +0000

    @task

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -1,9 +1,10 @@
+from flytekit import task
 
 """Application entry point."""
 from pathlib import Path
 import pandas as pd
 
-
+@task
 def run_package():
     print("yeet1")
     df = pd.read_csv('data/01_raw/small.csv')
