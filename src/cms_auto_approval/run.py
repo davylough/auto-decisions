cbc9ff21c53cdcbc16023e86819f9da8322e5a83
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-16 14:23:34 +0000

    get cwd

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -3,12 +3,15 @@ from flytekit import task
 """Application entry point."""
 from pathlib import Path
 import pandas as pd
+import os
 
 @task
 def run_package():
     print("yeet1")
-    df = pd.read_csv('~/data/01_raw/small.csv')
-    return df.head()
+    #df = pd.read_csv('~/data/01_raw/small.csv')
+    cwd = os.getcwd()
+    #return df.head()
+    return cwd
 
 
 if __name__ == "__main__":
