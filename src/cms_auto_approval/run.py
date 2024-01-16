cbdba91c2469ce097f3b7d6bdb02112c3abb4626
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-16 15:06:47 +0000

    print dir

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -9,14 +9,11 @@ import os
 @task
 def run_package():
     print("yeet1")
-    print(flytekit.current_context().working_directory)
-    csv_path = os.path.join(
-        flytekit.current_context().working_directory,
-        f"normalized-{os.path.basename(csv_url.path).rsplit('.')[0]}.csv",
-    )
-    df = pd.read_csv(csv_path)
-    print(df.head())
-    return df.head()
+    print(os.listdir('/auto-decisions'))
+    
+    #df = pd.read_csv(csv_path)
+    #print(df.head)
+    #return df.head()
 
 
 if __name__ == "__main__":
