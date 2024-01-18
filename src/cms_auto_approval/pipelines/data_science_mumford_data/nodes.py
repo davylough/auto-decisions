c97f708c9a3949e5fc568b91bcdbf4ec51875c7b
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-18 12:44:21 +0000

    sklearn import

--- src/cms_auto_approval/pipelines/data_science_mumford_data/nodes.py
+++ src/cms_auto_approval/pipelines/data_science_mumford_data/nodes.py
@@ -4,12 +4,11 @@ import pandas as pd
 import numpy as np
 import re
 import matplotlib.pyplot as plt
-import scikit-learn
-#from sklearn.metrics import classification_report
-#from sklearn.model_selection import GroupShuffleSplit
-#from sklearn.ensemble import GradientBoostingClassifier
-#from sklearn.metrics import log_loss
-#from sklearn.metrics import precision_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GroupShuffleSplit
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import log_loss
+from sklearn.metrics import precision_score
 import logging
 log = logging.getLogger(__name__)
 
