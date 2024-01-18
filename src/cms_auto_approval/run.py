cfe23a5aa8c946817a51f163614fb58470bb6fca
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-18 11:12:28 +0000

    typo

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -9,7 +9,7 @@ def run_package():
     df = pd.read_csv('auto-decisions/src/data/01_raw/small.csv')
     pre_pro_df = preprocess_mumford_data(df)
     model = train_model(pre_pro_df)
-    return modelx
+    return model
 
 
 if __name__ == "__main__":
