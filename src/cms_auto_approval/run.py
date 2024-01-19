b2ddd95d2a49f9b24ce5463b97466003bda7397b
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-19 12:17:54 +0000

    use large dataset

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -6,7 +6,7 @@ from src.cms_auto_approval.pipelines.data_science_mumford_data.nodes import trai
 
 @task
 def run_package():
-    df = pd.read_csv('auto-decisions/src/data/01_raw/small.csv')
+    df = pd.read_csv('auto-decisions/src/data/01_raw/200000.csv')
     pre_pro_df = preprocess_mumford_data(df)
     model = train_model(pre_pro_df)
     return model
