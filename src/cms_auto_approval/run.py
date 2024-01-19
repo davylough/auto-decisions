06fff75c704537d705c332ecd7a875a7b222b588
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-19 14:56:58 +0000

    pickle dump

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -1,5 +1,6 @@
 from flytekit import task
 import boto3
+import io
 from pathlib import Path
 import pandas as pd
 import pickle
@@ -8,15 +9,19 @@ from src.cms_auto_approval.pipelines.data_science_mumford_data.nodes import trai
 
 @task
 def run_package():
-    s3 = boto3.client("s3")
-    obj = s3.get_object(Bucket= "bv-ml-ops", Key= "pipelines/auto-decisions/200000.csv") 
+    s3_client = boto3.client("s3")
+    obj = s3_client.get_object(Bucket= "bv-ml-ops", Key= "pipelines/auto-decisions/200000.csv") 
     df = pd.read_csv(obj['Body'])
-
     pre_pro_df = preprocess_mumford_data(df)
     model = train_model(pre_pro_df)
-    pickle.dump(model, open("auto_decisions_model.pickle", "wb"))
-    loaded_model = pickle.load(open("auto_decisions_model.pickle", "rb"))
-    s3.put_object(Body= loaded_model, Bucket= "bv-ml-ops", Key= "pipelines/auto-decisions/model.onnx")
+
+    #pickle_buffer = io.BytesIO()
+    s3_resource = boto3.resource("s3")
+    #new_df.to_pickle(pickle_buffer)
+    
+    pickle_obj = pickle.dump(model, open("auto_decisions_model.pickle", "wb"))
+    s3_resource.Object("bv-ml-ops","pipelines/auto-decisions/model.onnx").put(Body=pickle_obj)
+    #s3.put_object(Body= loaded_model, Bucket= "bv-ml-ops", Key= "pipelines/auto-decisions/model.onnx")
     return model
 
 
