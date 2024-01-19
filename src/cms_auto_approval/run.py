9787fea5429cf0c424104a58c5aa247655650496
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-19 12:36:53 +0000

    pull csv from s3

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -1,4 +1,5 @@
 from flytekit import task
+import boto3
 from pathlib import Path
 import pandas as pd
 from src.cms_auto_approval.pipelines.data_engineering_mumford_data.nodes import preprocess_mumford_data
@@ -6,7 +7,12 @@ from src.cms_auto_approval.pipelines.data_science_mumford_data.nodes import trai
 
 @task
 def run_package():
-    df = pd.read_csv('auto-decisions/src/data/01_raw/200000.csv')
+    s3 = boto3.client("s3")
+    obj = s3.get_object(Bucket= "bv-ml-ops/pipelines/auto-decisions", Key= "200000.csv") 
+    df = pd.read_csv(obj['Body'])
+    print(df.head())
+
+    #df = pd.read_csv('auto-decisions/src/data/01_raw/200000.csv')
     pre_pro_df = preprocess_mumford_data(df)
     model = train_model(pre_pro_df)
     return model
