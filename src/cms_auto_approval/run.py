be8b4b997b2681205604f1681734cdd20f419eb5
David Loughridge <david.loughridge@bazaarvoice.com>
2024-01-19 15:30:56 +0000

    tidy up

--- src/cms_auto_approval/run.py
+++ src/cms_auto_approval/run.py
@@ -9,17 +9,21 @@ from src.cms_auto_approval.pipelines.data_science_mumford_data.nodes import trai
 
 @task
 def run_package():
+
+    # read training dataset from S3
     s3_client = boto3.client("s3")
     obj = s3_client.get_object(Bucket= "bv-ml-ops", Key= "pipelines/auto-decisions/200000.csv") 
     df = pd.read_csv(obj['Body'])
+
+    # pre-process data and train model
     pre_pro_df = preprocess_mumford_data(df)
     model = train_model(pre_pro_df)
 
+    # save model to S3
     s3_resource = boto3.resource("s3")
-    
     pickle_obj = pickle.dumps(model)
     s3_resource.Object("bv-ml-ops","pipelines/auto-decisions/model.onnx").put(Body=pickle_obj)
-    #s3.put_object(Body= loaded_model, Bucket= "bv-ml-ops", Key= "pipelines/auto-decisions/model.onnx")
+
     return model
 
 
